79a7d63a98efa0c312703bd0f5c2f0c6a6f1d034
opiu516 <martin.kostov12@gmail.com>
2018-07-05 11:52:41 +0300

    Main

---
+++ main.cc
@@ -0,0 +1,32 @@
+#ifndef _io_
+#include<iostream>
+#define _io_
+#endif
+#ifndef _fs_
+#include<fstream>
+#define _fs_
+#endif
+#ifndef _st_
+#include<string>
+#define _st_
+#endif
+
+using namespace std;
+
+bool running = true
+
+void make_choice(){
+    
+}
+
+void update_records(){
+    
+}
+
+int main(){
+    while(true){
+        make_choice();
+        upadte_records();
+    }
+    return 0
+}
