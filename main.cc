3737ed831a06217824917e52b36d347405c2ac00
vesko2624 <vesko_g@hotmail.com>
2018-07-05 12:41:40 +0300

    Smenih go 2

--- main.cc
+++ main.cc
@@ -1,17 +1,4 @@
-#ifndef _io_
-#include<iostream>
-#define _io_
-#endif
-#ifndef _fs_
-#include<fstream>
-#define _fs_
-#endif
-#ifndef _st_
-#include<string>
-#define _st_
-#endif
-
-using namespace std;
+#include "Airport.cpp"
 
 bool running = true;
 
