63860415697b8417f7b69ce44d66cd6956d5e8bc
LivingIron <cveganton@gmail.com>
2018-07-09 12:08:32 +0300

    More recorder tweaks

--- Headers/Recorder.cpp
+++ Headers/Recorder.cpp
@@ -15,16 +15,16 @@ Recorder::~Recorder(){
 void Recorder::Record_Flight(FlightInfo& temp){
 	Flight_Output_File <<temp.id_ << "  " << temp.route_.get_departure() << " " << temp.route_.get_arrival() << " " << temp.departure_time_.get_as_string() << " " << temp.arrival_time_.get_as_string() << " " << temp.price_ << endl;
 }
-FlightInfo Recorder::Read_Flight(){
-	FlightInfo temp;
+bool Recorder::Read_Flight(FlightInfo& temp){
 	int id,day_d,month_d,year_d,hour_d,minute_d,day_a,month_a,year_a,hour_a,minute_a;
 	double price;
 	string departure,arrival;
-	Input_File_Flights >> id >> departure >> arrival >> day_d >> month_d >> year_d >> hour_d >> minute_d >> day_a >> month_a >> year_a >> hour_a >> minute_a >> price;
+	if(!(Input_File_Flights >> id)) return false;
+	 Input_File_Flights >> departure >> arrival >> day_d >> month_d >> year_d >> hour_d >> minute_d >> day_a >> month_a >> year_a >> hour_a >> minute_a >> price;
 	temp.id_ = id;
 	temp.price_ = price;
 	temp.route_.set_route(departure,arrival);
 	temp.departure_time_.set_date(day_d,month_d,year_d,hour_d,minute_d);
 	temp.arrival_time_.set_date(day_a,month_a,year_a,hour_a,minute_a);
-	return temp;
+	return true;
 }
